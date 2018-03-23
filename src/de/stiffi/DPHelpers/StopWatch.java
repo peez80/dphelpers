8a232c7b0542380efb3ae2d6187a59d1f8c7e1d7
peez80 <peez@stiffi.de>
2018-03-23 18:41:02 +0100

    Add StopWatch (untested)

---
+++ src/de/stiffi/DPHelpers/StopWatch.java
@@ -0,0 +1,44 @@
+package de.stiffi.DPHelpers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class StopWatch {
+
+    private long totalCount;
+    private final AtomicLong processed = new AtomicLong(0);
+    private long startTime;
+
+    public StopWatch(long totalCount) {
+        this.totalCount = totalCount;
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public StopWatch() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public void increment() {
+        processed.incrementAndGet();
+    }
+
+    public void increment(long amount) {
+        processed.addAndGet(amount);
+    }
+
+    public long getProcessed() {
+        return processed.get();
+    }
+
+    public long getElapsedTimeMillis() {
+        return System.currentTimeMillis() - startTime;
+    }
+
+    public long getEstimatedRemainingTimeMillis() {
+        return getEstimatedTotalTimeMillis() - getElapsedTimeMillis();
+    }
+
+    public long getEstimatedTotalTimeMillis() {
+        double totalTime = ((double)totalCount * (double)getElapsedTimeMillis()) / processed.get();
+        return (long)totalTime;
+    }
+}
