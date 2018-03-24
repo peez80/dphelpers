d0b0ca525a721ff2012ef5a5487f2241fdcbac79
peez80 <peez@stiffi.de>
2018-03-24 11:44:29 +0100

    ObservableInputStream

--- src/de/stiffi/DPHelpers/IO/ObservableInputStream.java
+++ src/de/stiffi/DPHelpers/IO/ObservableInputStream.java
@@ -48,19 +48,6 @@ public class ObservableInputStream extends FilterInputStream {
         return result;
     }
 
-    @Override
-    public int read(byte[] b, int off, int len) throws IOException {
-        int result =  super.read(b, off, len);
-
-        if(result != -1) {
-            processedBytes.incrementAndGet();
-            informListener(false);
-        }else{
-            informListener(true);
-        }
-
-        return result;
-    }
 
     private void informListener(boolean force) {
         if (force || System.currentTimeMillis() - lastListenerInform > informIntervalMillis ) {
